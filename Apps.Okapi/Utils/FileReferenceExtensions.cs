2dec9062bf6cbe0714c71de8e76790a59d6af708
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-18 16:28:33 +0300

    File with weird names - maybe fixed

---
+++ Apps.Okapi/Utils/FileReferenceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Blackbird.Applications.Sdk.Common.Files;
+
+namespace Apps.Okapi.Utils;
+
+public static class FileReferenceExtensions
+{
+    public static string GetFileName(this FileReference fileReference)
+    {
+        return RemoveInappropriateCharacters(fileReference.Name);
+    }
+    
+    private static string RemoveInappropriateCharacters(string fileName)
+    {
+        if (fileName.StartsWith(@"\"))
+        {
+            fileName = fileName.Remove(0, 1);
+        }
+        
+        return fileName.Replace(@"\", "-");
+    }
+}
