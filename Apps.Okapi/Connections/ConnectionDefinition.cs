48d9f76d24ba507870da2b218aa4956a4f82e7d9
Alex Terehov <terales@users.noreply.github.com>
2025-09-20 23:09:32 -0230

    Revert connection changes

--- Apps.Okapi/Connections/ConnectionDefinition.cs
+++ Apps.Okapi/Connections/ConnectionDefinition.cs
@@ -2,27 +2,35 @@
 using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Connections;
 
-namespace Apps.Okapi.Connections;
-
 public class ConnectionDefinition : IConnectionDefinition
 {
-    
-    public IEnumerable<ConnectionPropertyGroup> ConnectionPropertyGroups =>
-    [
+    private static IEnumerable<ConnectionProperty> ConnectionProperties => new[]
+    {
+        new ConnectionProperty(CredsNames.Url)
+        {
+            DisplayName = "URL", Description = "Base URL of the Okapi API, f.e.: http://28.216.252.148:88/okapi-longhorn"
+        }
+    };
+
+    public IEnumerable<ConnectionPropertyGroup> ConnectionPropertyGroups => new List<ConnectionPropertyGroup>
+    {
         new()
         {
-            Name = "Base URL",
+            Name = "Developer API key",
             AuthenticationType = ConnectionAuthenticationType.Undefined,
-            ConnectionProperties = [
-                new ConnectionProperty(CredsNames.Url) { DisplayName = "URL", Description = "Base URL of the Okapi Longhorn server, e.g. http://28.216.252.148:88/okapi-longhorn" }
-            ],
+            ConnectionUsage = ConnectionUsage.Actions,
+            ConnectionProperties = ConnectionProperties
         }
-    ];
+    };
 
     public IEnumerable<AuthenticationCredentialsProvider> CreateAuthorizationCredentialsProviders(
         Dictionary<string, string> values)
     {
         var clientIdKeyValue = values.First(v => v.Key == CredsNames.Url);
-        yield return new AuthenticationCredentialsProvider( clientIdKeyValue.Key,clientIdKeyValue.Value);
+        yield return new AuthenticationCredentialsProvider(
+            AuthenticationCredentialsRequestLocation.None,
+            clientIdKeyValue.Key,
+            clientIdKeyValue.Value
+        );
     }
 }
