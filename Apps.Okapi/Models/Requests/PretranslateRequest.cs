2c33387225dc418e012b2b709ff5b5b5968c46d9
Alex Terehov <terales@users.noreply.github.com>
2025-09-21 00:26:18 -0230

    Update action description

--- Apps.Okapi/Models/Requests/PretranslateRequest.cs
+++ Apps.Okapi/Models/Requests/PretranslateRequest.cs
@@ -8,7 +8,7 @@ public class PretranslateRequest
     [Display("Content", Description = "File to be translated (source file).")]
     public FileReference File { get; set; } = new();
 
-    [Display("TM path on OKAPI server to TMX file or Pensieve TM folder)", Description = "Path to TM local to OKAPI server, use \"Upload translation assets\" action to obtain this path.")]
+    [Display("TM path on OKAPI server to TMX file or Pensieve TM folder)", Description = "Path to TM local to OKAPI server, use \"Create TM from TMX\" or \"Upload translation assets\" action to obtain this path.")]
     public string TmPath { get; set; } = string.Empty;
 
     [Display("Segmentation rules path on OKAPI server (SRX file)", Description = "Path to custom segmentation rules local to OKAPI server, use \"Upload translation assets\" action to obtain this path.")]
